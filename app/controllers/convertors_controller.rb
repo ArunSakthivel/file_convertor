6b7334f913e7809285e00276378de1ba9dfb7e9b
Arun <ArunSakthivel@users.noreply.github.com>
2018-06-18 21:18:44 +0530

    Update the file decode code

--- app/controllers/convertors_controller.rb
+++ app/controllers/convertors_controller.rb
@@ -1,47 +1,20 @@
 require 'libreconv'
 class ConvertorsController < ApplicationController
 
+  ##
+  # File encode code:
+  # Base64.encode64(File.read(<xlsx file path>))
   def create
-
-    #create a new tempfile named fileupload
-      tempfile = Tempfile.new("fileupload")
-      tempfile.binmode
-      # #get the file and decode it with base64 then write it to the tempfile
-      tempfile.write(Base64.decode64(params[:file_convertor]["file"]))
-
-     # File.open("#{Rails.root}/public/#{params[:file_convertor][:filename]}.xls",'wb'){ |f|
-     #  content = params[:file_convertor]["file"]
-     #  content.gsub!('\\r', "\r")
-     #  content.gsub!('\\n', "\n")
-     #  f.write(Base64.decode64(content))
-     #  f.close
-     # }
-
-    #check if file is within picture_path
-    # if params[:picture][:picture_path]["file"]
-    #   picture_path_params = params[:picture][:picture_path]
-    #   #create a new tempfile named fileupload
-    #   tempfile = Tempfile.new("fileupload")
-    #   tempfile.binmode
-    #   #get the file and decode it with base64 then write it to the tempfile
-    #   tempfile.write(Base64.decode64(picture_path_params["file"]))
-
-    #   #create a new uploaded file
-    #   uploaded_file = ActionDispatch::Http::UploadedFile.new(:tempfile => tempfile, :filename => picture_path_params["filename"], :original_filename => picture_path_params["original_filename"]) 
-
-    #   #replace picture_path with the new uploaded file
-    #   params[:picture][:picture_path] =  uploaded_file
-
-    # end
-
-    # @picture = Picture.new(params[:picture])
+    # File decoded and save it in the local path
+    File.open("#{Rails.root}/public/#{params[:file_convertor][:filename]}.xlsx", 'wb') do |f|
+      f.write(Base64.decode64(params[:file_convertor]["file"]))
+    end
 
     # file_conversion = FileConversion.new(tempfile)
     # download_file_path = file_conversion.convert
-
-    # p tempfile
-
-    Libreconv.convert(tempfile, "#{Rails.root}/public/file_conversion/document.pdf")
+    
+    # File conversion process from xlsx to pdf
+    Libreconv.convert("#{Rails.root}/public/#{params[:file_convertor][:filename]}.xlsx", "#{Rails.root}/public/file_conversion/document.pdf")
     download_file_path = "#{Rails.root}/public/file_conversion/document.pdf"
 
     
